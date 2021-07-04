16b0ffe09ccdcbd7b862a4b0426f0e969209970c
Hèctor <thehector1593@gmail.com>
2021-07-04 04:57:16 +0200

    FORK

--- subscriber/src/public/worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", event => {
-    const data = event.data.json();
-    console.log(data);
-    console.log("Got notification");
-
-    self.registration.showNotification(data.title, {
-        body: data.body,
-    })
-})
